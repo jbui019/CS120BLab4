2f48e457f15c150f71c902b90a13c07fd8a015ae
Jarvis Bui <jbui019@wch136-06.cs.ucr.edu>
2020-04-19 16:23:45 -0700

    test4

--- source/main.c
+++ source/main.c
@@ -10,43 +10,64 @@
 #include <avr/io.h>
 #ifdef _SIMULATE_
 #include "simAVRHeader.h"
-enum States{Start, standBy, increment, decrement, reset}state;
-unsigned char count, tempA;
+enum States{Start, INIT,  standBy, increment, decrement, reset}state;
 #endif
 
 
 void tick(){
 	switch(state){//transitions
 		case Start:
-			state = standBy;
+			PORTC = 0x07;
+			state = INIT;
 			break;
 
-		default:
-			state = Start;
+		case INIT:
+			if(PINA == 0x01){
+				state = increment;
+			}
+			else if(PINA == 0x02){
+				state = decrement;
+			}
+			else if(PINA == 0x03){
+				state = reset;
+			}
+			else{
+				state = INIT;
+			}
 			break;
 
 		case standBy:
-			if(tempA == 0x03){
+			if((PINA == 0x01) || (PINA == 0x02)){
+				state = standBy;
+			}
+			else if(PINA == 0x03){
 				state = reset;
 			}
-			else if(tempA == 0x01 && count < 9){
-				state = increment;
-			}
-			else if(tempA == 0x02 && count > 0){
-				state = decrement;
+			else{
+				state = INIT;
 			}
 			break;
 
 		case reset:
-			state = standBy;
+			if((PINA == 0x01) || (PINA == 0x02)){
+				state = reset;
+			}
+			else{
+				state = INIT;
+			}
+			
 			break;
 
 		case increment:
 			state = standBy;
+		
 			break;
 
 		case decrement:
-			state = standBy; 
+			state = standBy;
+			
+			break;
+		default:
 			break;
 	}
 
@@ -55,22 +76,33 @@ void tick(){
 			break;
 
 		case Start:
-			count = 7;
+			PORTC = 0x07;
 			break;
-
+		case INIT:
+			break;
 		case standBy:
 			break;
 
 		case reset:
-			count = 0;
+			PORTC = 0x00;
 			break;
 
 		case increment:
-			count++;
+			if(PORTC >= 0x09){
+				PORTC = 0x09;
+			}
+			else{
+				PORTC = PORTC + 0x01;
+			}
 			break;
 
 		case decrement:
-			count--;
+			if(PORTC <= 0x00){
+				PORTC = 0x00;
+			}
+			else{
+				PORTC = PORTC - 0x01;
+			}
 			break;
 	}
 
@@ -79,13 +111,11 @@ void tick(){
 int main(void) {
     /* Insert DDR and PORT initializations */
 	DDRA = 0x00; PORTA = 0xFF;
-	DDRC = 0xFF; PORTC = 0x00;
+	DDRC = 0xFF; PORTC = 0x07;
     /* Insert your solution below */
 	state = Start;
-    while (1) {
-	tempA = PINA;
-	tick();	
-	PORTC = count;
-    }
+ 	while (1) {	
+		tick();	
+ 	}
     return 1;
 }
--- test/tests.gdb
+++ test/tests.gdb
@@ -27,25 +27,72 @@ echo ======================================================\n
 echo Running all tests..."\n\n
 
 # Add tests below
+test "PINA: 0x00 => PORTC: 7"
+set state = Start
+setPINA 0x00
+continue 2
+expectPORTC 0x07
+checkResult
 
-test "PINA: 0x03,0x01, 0x01, 0x01, 0x01, 0x02, 0x02 => C = 2"
+test "PINA: 0x01 => PORTC: 8"
+set state = Start
+setPINA 0x01
+continue 2
+expectPORTC 0x08
+checkResult
+
+test "PINA: 0x01, 0x01, PINA: 0x00 => PORTC: 9"
+set state = Start
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2
+expectPORTC 0x09
+checkResult
+
+test "PINA: 0x02 => PORTC: 6"
+set state = Start
+setPINA 0x02
+continue 2
+setPINA 0x00
+continue 2
+expectPORTC 0x06
+checkResult
+
+test "PINA: 0x03 => PORTC: 0"
 set state = Start
 setPINA 0x03
 continue 2
-setPINA 0x01
-continue 2
-setPINA 0x01
-continue 2
-setPINA 0x01
-continue 2
-setPINA 0x01
+setPINA 0x00
 continue 2
 setPINA 0x02
 continue 2
-setPINA 0x02
+setPINA 0x00
 continue 2
-expectPORTC 0x02
+expectPORTC 0x00
 checkResult
+
+test "increment at max"
+set state = Start
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2 
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2
+setPINA 0x01
+continue 2
+setPINA 0x00
+continue 2
+expectPORTC 0x09
+checkResult
+
 
 
 # Report on how many tests passed/testso ran
